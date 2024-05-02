8f9a71c9f157d4a802b720b0f05194c85ccad73f
Shangxpss <270291223@qq.com>
2024-05-02 12:45:59 +0800

    123

--- blog/countNum/countNumber.py
+++ blog/countNum/countNumber.py
@@ -1,10 +1,12 @@
 from collections import Counter
 from docx import Document
+from pathlib import Path
+import os
 
-
-def cuntWord(path: str):
-    document = Document("passage.docx")
-    para = document.add_paragraph("This is new")
+def cuntWord(name:str):
+    fileList = getpath(name)
+    # assert os.path.isfile(fileList[0])
+    document = Document("new.docx")
 
     paragraph = document.paragraphs
     text:list[str] = []
@@ -14,6 +16,15 @@ def cuntWord(path: str):
     result = Counter(text)
     print(result)
 
+def getpath(name:str):
+    cwd = Path.cwd()
+    dirPath = os.path.join(cwd.parent,name)
+    allFile = os.listdir(dirPath)
+    fileList:list[str] = []
+    for name in allFile:
+        fullName = os.path.join(dirPath,name)
+        fileList.append(fullName)
+    return fileList
 
 if __name__ == "__main__":
-    cuntWord("")
+    cuntWord("store")
